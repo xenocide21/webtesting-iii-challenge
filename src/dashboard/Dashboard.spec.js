abe3a6953987c8d63e2e1f3d69a17c8a57e54344
Ashley Dunham <xenocide21@gmail.com>
2019-12-16 12:40:00 -0800

    Display and Dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
 // Test away
+import { render } from '@testing-library/react';
+import React from 'react'
+import Dashboard from './Dashboard.js'
+import Display from '../display/Display.js'
+import Controls from '../controls/Controls.js'
+
+test('Is Display On', () => {
+    render(<Display />);
+});
+
+test('Are Controls On', () => {
+    render(<Controls />);
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,49 @@
 // Test away!
+import React from "react";
+import { render } from '@testing-library/react'
+import Display from "./Display";
+
+
+test('Is Gate Open Or Closed, Is Gate Locked Or Unlocked',()=> {
+    const locked = false
+    const closed = false
+    const display = render(<Display locked={locked} closed={closed}/>)
+    display.getByText(/unlocked/i)
+    display.getByText(/open/i)
+})
+
+test('Is Gate Closed', ()=>{
+    const closed = true
+    const display = render(<Display closed={closed}/>)
+    display.getByText(/closed/i)
+})
+
+test('Is Gate Locked', ()=>{
+    const locked = true
+    const display = render(<Display locked={locked}/>)
+    display.getByText(/locked/i)
+})
+
+test('Is Light Green', ()=>{
+    const locked = false
+    const closed = false
+
+    const display = render(<Display locked={locked} closed={closed}/>)
+    const unlocked = display.getByText(/unlocked/i)
+    const open = display.getByText(/open/i)
+
+    expect(unlocked.classList.contains('green-led')).toBe(true)
+    expect(open.classList.contains('green-led')).tobe(true)
+})
+
+test('Is Light Red', ()=>{
+    const locked = true
+    const closed = true
+
+    const display = render(<Display locked={locked} closed={closed}/>)
+    const unlocked = display.getByText(/unlocked/i)
+    const open = display.getByText(/open/i)
+
+    expect(unlocked.classList.contains('red-led')).toBe(true)
+    expect(open.classList.contains('red-led')).tobe(true)
+})
